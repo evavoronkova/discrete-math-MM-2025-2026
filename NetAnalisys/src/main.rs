2439e3c8d29d21aed64b3de1e530f16c4f1f197b
Vladislav <morozvv75@gmail.com>
2026-04-19 14:43:25 +0300

    fix(parser): now isolated nodes are counted

--- NetAnalisys/src/main.rs
+++ NetAnalisys/src/main.rs
@@ -1,6 +1,21 @@
 mod parser;
 mod graph;
+
+
+#[allow(dead_code)]
+fn calculate_main_graph_information(graph: &graph::Graph) {
+    let num_vertices = graph.adjacency_list.len();
+    let mut num_edges: usize = graph
+        .adjacency_list
+        .values()
+        .map(|neighbors| neighbors.len())
+        .sum();
+
+    if let parser::directed_or_undirected::DirectedOrUndirected::Undirected = graph.graph_type {
+        num_edges /= 2;
+    }
+}
 fn main() {
-    
+
 
 }
