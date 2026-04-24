e31d04a5ab930de0cc344b39e9699b5d0696e967
Andrey Ovechkin <andryu.ovechkin@gmail.com>
2026-04-24 21:36:24 +0300

    feat: complete functions for checking robustness

--- NetAnalisys/src/analysis/robustness.rs
+++ NetAnalisys/src/analysis/robustness.rs
@@ -6,6 +6,7 @@ use crate::{
     parser::directed_or_undirected::DirectedOrUndirected,
 };
 
+use rand::seq::SliceRandom;
 use std::collections::{HashMap, HashSet};
 
 fn remove_vertices(graph: &Graph, to_remove: &HashSet<u32>) -> Graph {
@@ -62,5 +63,36 @@ fn lcc_after_hub_removal(graph: &Graph) -> HashMap<u32, f64> {
         hashmap.insert(percent as u32, fraction);
     }
 
+    hashmap
+}
+
+fn lcc_after_random_removal(graph: &Graph, trials: usize) -> HashMap<u32, f64> {
+    let mut hashmap: HashMap<u32, f64> = HashMap::new();
+    for x in 1..=20 {
+        let percent = x * 5;
+        let num_vertices_to_delete =
+            ((percent * graph.num_vertices()) as f64 / 100.0).round() as u32;
+        let num_to_remove = if num_vertices_to_delete > graph.num_vertices() as u32 {
+            graph.num_vertices() as u32
+        } else {
+            num_vertices_to_delete
+        };
+
+        let mut rng = rand::thread_rng();
+        let mut mid_fraction: f64 = 0.0;
+        for _ in 0..trials {
+            let mut vertices: Vec<u32> = graph.adjacency_list.keys().cloned().collect();
+            vertices.shuffle(&mut rng);
+            let to_remove: HashSet<u32> =
+                vertices.into_iter().take(num_to_remove as usize).collect();
+
+            let new_graph = remove_vertices(graph, &to_remove);
+            let mut comps = find_weak_components(&new_graph);
+            let fraction = fraction_in_largest_component(&mut comps, new_graph.num_vertices());
+            mid_fraction += fraction;
+        }
+        mid_fraction /= trials as f64;
+        hashmap.insert(percent as u32, mid_fraction);
+    }
     hashmap
 }
