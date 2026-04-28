ba9527b62d69c613770c0b8ae32d90b9d409a204
Andrew_Baranov <andybaranov07@gmail.com>
2026-04-28 22:16:52 +0300

    perf(2977): replace Trie with substring slicing and optimize Floyd-Warshall

--- tasks/2977.py
+++ tasks/2977.py
@@ -16,20 +16,20 @@ class Solution:
         for i in range(len(original)):
             old, new, c = original[i], changed[i], cost[i]
             j, k = substr_id[old], substr_id[new]
-            costs[j][k] = min(costs[j][k], c)
+            if c < costs[j][k]:
+                costs[j][k] = c
 
         for k in range(substr_num):
+            cost_k = costs[k]
             for i in range(substr_num):
+                cost_i = costs[i]
+                if cost_i[k] == INF:
+                    continue
                 for j in range(substr_num):
-                    costs[i][j] = min(costs[i][j], costs[i][k] + costs[k][j])
+                    if cost_i[k] + cost_k[j] < cost_i[j]:
+                        cost_i[j] = cost_i[k] + cost_k[j]
 
-        trie = {}
-        for substring in substrings:
-            node = trie
-            for char in substring:
-                node = node.setdefault(char, {})
-            node['id'] = substr_id[substring]
-
+        lengths = sorted(list(set(len(s) for s in original)))
 
         num = len(source)
         dynamic_costs = [INF] * (num + 1)
@@ -39,19 +39,22 @@ class Solution:
             if dynamic_costs[i] == INF:
                 continue
             if source[i] == target[i]:
-                dynamic_costs[i + 1] = min(dynamic_costs[i + 1], dynamic_costs[i])
+                if dynamic_costs[i] < dynamic_costs[i + 1]:
+                    dynamic_costs[i + 1] = dynamic_costs[i]
 
-            old = trie
-            new = trie
-            for j in range(i, num):
-                if source[j] not in old or target[j] not in new:
+            for length in lengths:
+                if i + length > num:
                     break
-                old = old[source[j]]
-                new = new[target[j]]
+                old = source[i: i + length]
+                new = target[i: i + length]
 
-                if 'id' in old and 'id' in new:
-                    k1, k2 = old['id'], new['id']
-                    if costs[k1][k2] != INF:
-                        dynamic_costs[j + 1] = min(dynamic_costs[j + 1], dynamic_costs[i] + costs[k1][k2])
+                j = substr_id.get(old)
+                k = substr_id.get(new)
+
+                if j is not None and k is not None:
+                    cost_jk = costs[j][k]
+                    if cost_jk != INF:
+                        if dynamic_costs[i] + cost_jk < dynamic_costs[i + length]:
+                            dynamic_costs[i + length] = dynamic_costs[i] + cost_jk
 
         return dynamic_costs[num] if dynamic_costs[num] != INF else -1
