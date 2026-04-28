c9458ef13b758f7d19b309a96e307df1278ef66f
Andrew_Baranov <andybaranov07@gmail.com>
2026-04-28 16:22:47 +0300

    fix(1466): use copy of connections

--- tasks/1466.py
+++ tasks/1466.py
@@ -1,18 +1,18 @@
-from collections import defaultdict
-
-
 class Solution:
     def minReorder(self, n: int, connections: list[list[int]]) -> int:
-        graph = defaultdict(dict)
+        roads = connections.copy()
         to_cities = [0]
         min_changed = 0
 
-
-        for a, b in connections:
-            if a in to_cities:
-                min_changed += 1
-                to_cities.append(b)
-            elif b in to_cities:
-                to_cities.append(a)
+        while roads:
+            for pair in roads:
+                a, b = pair
+                if a in to_cities:
+                    min_changed += 1
+                    to_cities.append(b)
+                    roads.remove(pair)
+                elif b in to_cities:
+                    to_cities.append(a)
+                    roads.remove(pair)
 
         return min_changed
