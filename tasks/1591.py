ae7d51769261f4ecd0b19e2b3784d5a97a74099c
Andrew_Baranov <andybaranov07@gmail.com>
2026-04-29 20:44:50 +0300

    perf(1591): optimize solution removing recursion

--- tasks/1591.py
+++ tasks/1591.py
@@ -1,54 +1,59 @@
-import sys
-
 class Solution:
     def isPrintable(self, targetGrid: list[list[int]]) -> bool:
-        sys.setrecursionlimit(2000)
         rows, cols = len(targetGrid), len(targetGrid[0])
 
-        squares = [None] * 61
+        squares = [-1] * 244  # Оптимизация памяти: вместо списков в squares подряд хранятся цвета углов прямоугольников
         found_colours = []
         for row in range(rows):
             row_cells = targetGrid[row]
             for col in range(cols):
                 colour = row_cells[col]
-                if squares[colour] is None:
-                    squares[colour] = [row, row, col, col]
+                i = colour * 4
+                if squares[i] == -1:
+                    squares[i:i + 4] = [row, row, col, col]
                     found_colours.append(colour)
                 else:
-                    square = squares[colour]
-                    if row < square[0]:
-                        square[0] = row
-                    if row > square[1]:
-                        square[1] = row
-                    if col < square[2]:
-                        square[2] = col
-                    if col > square[3]:
-                        square[3] = col
+                    if row < squares[i]:
+                        squares[i] = row
+                    if row > squares[i + 1]:
+                        squares[i + 1] = row
+                    if col < squares[i + 2]:
+                        squares[i + 2] = col
+                    if col > squares[i + 3]:
+                        squares[i + 3] = col
 
-        dependencies = [set() for _ in range(61)]
+        dependencies = [[] for _ in range(61)]
         for colour in found_colours:
-            upper_row, lower_row, left_col, right_col = squares[colour]
-            for row in range(upper_row, lower_row + 1):
+            i = colour * 4
+            for row in range(squares[i], squares[i + 1] + 1):
                 row_cells = targetGrid[row]
-                for col in range(left_col, right_col + 1):
+                for col in range(squares[i + 2], squares[i + 3] + 1):
                     inner_colour = row_cells[col]
-                    if inner_colour != colour:
-                        dependencies[colour].add(inner_colour)
+                    if inner_colour != colour and inner_colour not in dependencies[colour]:
+                        dependencies[colour].append(inner_colour)
 
         cycle_state = [0] * 61
-        def has_cycle(colour: int) -> bool:
-            cycle_state[colour] = 1
-            for inner_colour in dependencies[colour]:
-                if cycle_state[inner_colour] == 1:
-                    return True
-                if not cycle_state[inner_colour] and has_cycle(inner_colour):
-                    return True
-            cycle_state[colour] = 2
-            return False
 
         for colour in found_colours:
             if not cycle_state[colour]:
-                if has_cycle(colour):
-                    return False
+                stack = [(colour, 0)]
+                while stack:
+                    inner_colour, i = stack[-1]
+                    if i == 0:
+                        cycle_state[inner_colour] = 1
 
+                    found_next = False
+                    for j in range(i, len(dependencies[inner_colour])):
+                        next = dependencies[inner_colour][j]
+                        if cycle_state[next] == 1:
+                            return False
+                        if not cycle_state[next]:
+                            stack[-1] = (inner_colour, j + 1)
+                            stack.append((next, 0))
+                            found_next = True
+                            break
+
+                    if not found_next:
+                        cycle_state[inner_colour] = 2
+                        stack.pop()
         return True
