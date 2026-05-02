e812273ecf94466d01bd050343aaa78a7a2ed66f
Nikita Kabanov <kabanovnikita54@gmail.com>
2026-05-03 00:15:06 +0300

    solved 947

---
+++ solutions/947.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+
+def removeStones(stones):
+    rows = defaultdict(list)
+    cols = defaultdict(list)
+    visited = set()
+    groups = 0
+
+    for i in range(len(stones)):
+        r, c = stones[i]
+        rows[r].append(i)
+        cols[c].append(i)
+
+    def dfs(i):
+        visited.add(i)
+        r, c = stones[i]
+
+        for j in rows[r]:
+            if j not in visited:
+                dfs(j)
+
+        for j in cols[c]:
+            if j not in visited:
+                dfs(j)
+    
+    for i in range(len(stones)):
+        if i not in visited:
+            dfs(i)
+            groups += 1
+
+    return len(stones) - groups
+
+
+print(removeStones([[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]))
