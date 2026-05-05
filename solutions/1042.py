3544dbc36f6ff58a0d63e9469d618e1f4e32e927
Nikita Kabanov <kabanovnikita54@gmail.com>
2026-05-05 23:29:32 +0300

    solution 1042

--- solutions/1042.py
+++ solutions/1042.py
@@ -1,18 +1,19 @@
-def gardenNoAdj(n: int, paths: list[list[int]]) -> list[int]:
-    graph = [[] for _ in range(n)]
-    
-    for u, v in paths:
-        graph[u - 1].append(v - 1)
-        graph[v - 1].append(u - 1)
+class Solution:
+    def gardenNoAdj(self, n: int, paths):
+        graph = [[] for _ in range(n)]
         
-    res = [0] * n
-    
-    for i in range(n):
-        used_colors = {res[neighbor] for neighbor in graph[i]}
+        for u, v in paths:
+            graph[u - 1].append(v - 1)
+            graph[v - 1].append(u - 1)
+            
+        res = [0] * n
         
-        for color in range(1, 5):
-            if color not in used_colors:
-                res[i] = color
-                break
-                
-    return res
+        for i in range(n):
+            used_colors = {res[neighbor] for neighbor in graph[i]}
+            
+            for color in range(1, 5):
+                if color not in used_colors:
+                    res[i] = color
+                    break
+                    
+        return res
