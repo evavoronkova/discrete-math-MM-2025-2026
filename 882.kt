83cb28d4d997d212c92133f5f2e6b897e887f8fd
Горбузова Мария <gorbuzova.maria@yandex.ru>
2026-05-10 18:00:53 +0300

    882

---
+++ 882.kt
@@ -0,0 +1,69 @@
+class Solution {
+    fun reachableNodes(edges: Array<IntArray>, maxMoves: Int, n: Int): Int {
+        // Считаем степени вершин для точного размера массивов (CSR-формат)
+        val nodeDegree = IntArray(n)
+        for (edge in edges) {
+            nodeDegree[edge[0]]++
+            nodeDegree[edge[1]]++
+        }
+
+        val adjacencyList = Array(n) { nodeIndex -> IntArray(nodeDegree[nodeIndex]) }
+        val fillPointer = IntArray(n)
+        for (edge in edges) {
+            val sourceNode = edge[0]
+            val destinationNode = edge[1]
+            val subNodeCount = edge[2]
+            // Упаковываем (subNodeCount, сосед) в один Int
+            adjacencyList[sourceNode][fillPointer[sourceNode]++] = (subNodeCount shl 12) or destinationNode
+            adjacencyList[destinationNode][fillPointer[destinationNode]++] = (subNodeCount shl 12) or sourceNode
+        }
+
+        val distanceFromStart = IntArray(n) { Int.MAX_VALUE }
+        distanceFromStart[0] = 0
+        val priorityQueue = java.util.PriorityQueue<Long>()
+        priorityQueue.add(0L) // distance=0, node=0
+        while (priorityQueue.isNotEmpty()) {
+            val packedEntry = priorityQueue.poll()
+            val currentDistance = (packedEntry ushr 12).toInt()
+            val currentNode = (packedEntry and 0xFFFL).toInt()
+            if (currentDistance > distanceFromStart[currentNode]) {
+                continue
+            }
+            for (packedNeighbor in adjacencyList[currentNode]) {
+                val neighborNode = packedNeighbor and 0xFFF
+                val subNodeCount = packedNeighbor ushr 12
+                val newDistance = currentDistance + subNodeCount + 1
+                if (newDistance < distanceFromStart[neighborNode]) {
+                    distanceFromStart[neighborNode] = newDistance
+                    priorityQueue.add((newDistance.toLong() shl 12) or neighborNode.toLong())
+                }
+            }
+        }
+
+        var totalReachableNodes = 0
+        for (nodeIndex in 0 until n) {
+            if (distanceFromStart[nodeIndex] <= maxMoves) {
+                totalReachableNodes++
+            }
+        }
+
+        for (edge in edges) {
+            val sourceNode = edge[0]
+            val destinationNode = edge[1]
+            val subNodeCount = edge[2]
+            val reachableFromSource = if (distanceFromStart[sourceNode] <= maxMoves) {
+                maxMoves - distanceFromStart[sourceNode]
+            } else {
+                0
+            }
+            val reachableFromDestination = if (distanceFromStart[destinationNode] <= maxMoves) {
+                maxMoves - distanceFromStart[destinationNode]
+            } else {
+                0
+            }
+
+            totalReachableNodes += minOf(subNodeCount, reachableFromSource + reachableFromDestination)
+        }
+        return totalReachableNodes
+    }
+}
