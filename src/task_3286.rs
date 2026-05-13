80621cae75b6bc96539f62a04459dbb29f047094
Andrey Ovechkin <andryu.ovechkin@gmail.com>
2026-05-13 15:41:05 +0300

    perf(3286): simplify neighbour iteration (Accepted, 9 ms -> 4 ms, 2.47 MB -> 2.26 MB)

--- src/task_3286.rs
+++ src/task_3286.rs
@@ -2,56 +2,33 @@ use std::collections::VecDeque;
 
 impl Solution {
     pub fn find_safe_walk(grid: Vec<Vec<i32>>, health: i32) -> bool {
-        let n = grid.len() as i32;
-        let m = grid[0].len() as i32;
+        let n = grid.len();
+        let m = grid[0].len();
         let health = health - grid[0][0];
+        if health <= 0 {
+            return false;
+        }
 
-        let mut healths = vec![vec![0; m as usize]; n as usize];
-        let mut queue: VecDeque<Vec<i32>> = VecDeque::new();
+        let mut healths: Vec<Vec<i32>> = vec![vec![-1; m]; n];
+        let vars = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+        let mut queue: VecDeque<(i32, i32, i32)> = VecDeque::new();
+
         healths[0][0] = health;
-        queue.push_back(vec![0, 0, health]);
-        while let Some(info) = queue.pop_front() {
-            let current_x = info[0];
-            let current_y = info[1];
-            let current_health = info[2];
-            if current_x == n - 1 && current_y == m - 1 {
+        queue.push_back((0, 0, health));
+
+        while let Some((current_x, current_y, current_health)) = queue.pop_front() {
+            if current_x == n as i32 - 1 && current_y == m as i32 - 1 {
                 return true;
             }
-
-            if current_x - 1 >= 0 {
-                let new_health = current_health - grid[current_x as usize - 1][current_y as usize];
-                if new_health > 0
-                    && new_health > healths[current_x as usize - 1][current_y as usize]
-                {
-                    queue.push_back(vec![current_x - 1, current_y, new_health]);
-                    healths[current_x as usize - 1][current_y as usize] = new_health;
-                }
-            }
-            if current_y - 1 >= 0 {
-                let new_health = current_health - grid[current_x as usize][current_y as usize - 1];
-                if new_health > 0
-                    && new_health > healths[current_x as usize][current_y as usize - 1]
-                {
-                    queue.push_back(vec![current_x, current_y - 1, new_health]);
-                    healths[current_x as usize][current_y as usize - 1] = new_health;
-                }
-            }
-            if current_x + 1 < n {
-                let new_health = current_health - grid[current_x as usize + 1][current_y as usize];
-                if new_health > 0
-                    && new_health > healths[current_x as usize + 1][current_y as usize]
-                {
-                    queue.push_back(vec![current_x + 1, current_y, new_health]);
-                    healths[current_x as usize + 1][current_y as usize] = new_health;
-                }
-            }
-            if current_y + 1 < m {
-                let new_health = current_health - grid[current_x as usize][current_y as usize + 1];
-                if new_health > 0
-                    && new_health > healths[current_x as usize][current_y as usize + 1]
-                {
-                    queue.push_back(vec![current_x, current_y + 1, new_health]);
-                    healths[current_x as usize][current_y as usize + 1] = new_health;
+            for (dx, dy) in vars.iter() {
+                let new_x = current_x + dx;
+                let new_y = current_y + dy;
+                if new_x >= 0 && new_y >= 0 && new_x < n as i32 && new_y < m as i32 {
+                    let new_health = current_health - grid[new_x as usize][new_y as usize];
+                    if new_health > 0 && new_health > healths[new_x as usize][new_y as usize] {
+                        healths[new_x as usize][new_y as usize] = new_health;
+                        queue.push_back((new_x, new_y, new_health));
+                    }
                 }
             }
         }
