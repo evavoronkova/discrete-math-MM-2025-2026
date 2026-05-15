8b19c736d2fef418c80b5ccb0a985ba964ca7900
Nikita Kabanov <kabanovnikita125@gmail.com>
2026-05-15 15:27:58 +0300

    feat(benchmark): implementation DistanceEstimatorQuality.kt

---
+++ app/src/main/kotlin/benchmark/DistanceEstimatorQuality.kt
@@ -0,0 +1,22 @@
+package benchmark
+
+data class DistanceEstimatorQuality(
+    val avgError: Double,
+    val maxError: Int,
+    val avgRelativeError: Double,
+    val exactMatches: Int,
+    val totalQueries: Int,
+    val exactMatchRate: Double
+) {
+    override fun toString(): String {
+        return """
+            --- Result tests ---
+            Total requests: $totalQueries
+            Exact matches: $exactMatches (${String.format("%.2f", exactMatchRate * 100)}%)
+            Average error (edges): ${String.format("%.3f", avgError)}
+            Maximum error (edges): $maxError
+            Average relative error: ${String.format("%.2f", avgRelativeError * 100)}%
+            -------------------------------
+        """.trimIndent()
+    }
+}
