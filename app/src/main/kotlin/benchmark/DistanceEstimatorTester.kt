aff2801f9ca123d27c36634e2906257cfcc000aa
Nikita Kabanov <kabanovnikita125@gmail.com>
2026-05-15 16:38:38 +0300

    feat(benchmark): implement DistanceEstimatorTester to measure LCA and Basic algorithms accuracy

---
+++ app/src/main/kotlin/benchmark/DistanceEstimatorTester.kt
@@ -0,0 +1,68 @@
+package benchmark
+
+import core.model.Graph
+import landmarks.DistanceEstimator
+import kotlin.math.abs
+
+class DistanceEstimatorTester(
+    private val graph: Graph,
+    private val estimator: DistanceEstimator
+) {
+
+    fun test(numPairs: Int): DistanceEstimatorQuality {
+        val pairs = mutableListOf<Pair<Int, Int>>()
+        val vertexCount = graph.vertexCount
+
+        repeat(numPairs) {
+            val u = (0 until vertexCount).random()
+            val v = (0 until vertexCount).random()
+            pairs.add(Pair(u, v))
+        }
+
+        return testWithPairs(pairs)
+    }
+
+    fun testWithPairs(pairs: List<Pair<Int, Int>>): DistanceEstimatorQuality {
+        var totalError = 0.0
+        var totalRelativeError = 0.0
+        var maxError = 0
+        var exactMatches = 0
+        var validQueries = 0
+
+        estimator.preprocess()
+
+        for ((u, v) in pairs) {
+            if (u == v) continue
+
+            val exactDist = estimator.exactDistance(u, v)
+            
+            val estimatedDist = estimator.estimateDistance(u, v)
+
+            if (exactDist != -1 && estimatedDist != -1) {
+                validQueries++
+                
+                val error = abs(estimatedDist - exactDist)
+                val relativeError = error.toDouble() / exactDist.toDouble()
+
+                totalError += error
+                totalRelativeError += relativeError
+                
+                if (error > maxError) maxError = error
+                if (error == 0) exactMatches++
+            }
+        }
+
+        if (validQueries == 0) {
+            return DistanceEstimatorQuality(0.0, 0, 0.0, 0, 0, 0.0)
+        }
+
+        return DistanceEstimatorQuality(
+            avgError = totalError / validQueries,
+            maxError = maxError,
+            avgRelativeError = totalRelativeError / validQueries,
+            exactMatches = exactMatches,
+            totalQueries = validQueries,
+            exactMatchRate = exactMatches.toDouble() / validQueries
+        )
+    }
+}
