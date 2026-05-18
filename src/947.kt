9468d6fa91e5e801c199b3654262dcd3f2c0cb3c
kevetka <redpatrik2007@gmail.com>
2026-05-18 15:38:35 +0300

    Adding unsaved solution 947

--- src/947.kt
+++ src/947.kt
@@ -1,3 +1,39 @@
-fun main() {
-    val input =
+class Solution {
+    fun removeStones(stones: Array<IntArray>): Int {
+        val offset = 10001
+
+        val parent = IntArray(20002) { it }
+        val size = IntArray(20002) { 1 }
+
+        fun find(x: Int): Int {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x])
+            }
+            return parent[x]
+        }
+
+        fun union(x: Int, y: Int) {
+            var rootX = find(x)
+            var rootY = find(y)
+
+            if (rootX == rootY) return
+
+            if (size[rootX] < size[rootY]) {
+                val temp = rootX
+                rootX = rootY
+                rootY = temp
+            }
+
+            parent[rootY] = rootX
+            size[rootX] += size[rootY]
+        }
+
+        for ((r, c) in stones) {
+            union(r,c + offset)
+        }
+
+        val components = stones.map { find(it[0]) }.toSet().size
+
+        return stones.size - components
+    }
 }
