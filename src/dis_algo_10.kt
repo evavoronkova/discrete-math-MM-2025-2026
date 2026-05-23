79f2455d57a353498b33b9e6ad0db377fb43c55c
Narysev <narysevsergej735@gmail.com>
2026-05-23 12:24:37 +0300

    2360 optimize cycle detection traversal

--- src/dis_algo_10.kt
+++ src/dis_algo_10.kt
@@ -6,6 +6,8 @@ class Solution {
 
         val used = BooleanArray(n)
 
+        val dist = IntArray(n) { -1 }
+
         var ans = -1
 
         for (i in 0 until n) {
@@ -17,23 +19,27 @@ class Solution {
             var v = i
             var step = 0
 
-            val pos = HashMap<Int, Int>()
-
             while (v != -1 && !used[v]) {
 
                 used[v] = true
-
-                pos[v] = step++
+                dist[v] = step++
 
                 v = edges[v]
             }
 
-            if (v != -1 && pos.containsKey(v)) {
+            if (v != -1 && dist[v] != -1) {
+                ans = maxOf(ans, step - dist[v])
+            }
 
-                ans = maxOf(
-                    ans,
-                    step - pos[v]!!
-                )
+            v = i
+
+            while (v != -1 && dist[v] != -1) {
+
+                val nxt = edges[v]
+
+                dist[v] = -1
+
+                v = nxt
             }
         }
 
