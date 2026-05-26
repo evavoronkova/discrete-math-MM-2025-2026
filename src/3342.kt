46d422fb294b24edd6b92c917dedb89b904158d6
kevetka <redpatrik2007@gmail.com>
2026-05-26 05:06:24 +0300

    Improved solution 3342

--- src/3342.kt
+++ src/3342.kt
@@ -5,39 +5,41 @@ class Solution {
         val n = moveTime.size
         val m = moveTime[0].size
 
-        val dist = Array(n) { Array(m) { intArrayOf(Int.MAX_VALUE, Int.MAX_VALUE) } }
-        dist[0][0][0] = 0
+        val dist = IntArray(n * m * 2) { Int.MAX_VALUE }
+        dist[0] = 0
 
         val pq = java.util.PriorityQueue<State>(compareBy { it.time })
         pq.add(State(0, 0, 0, 0))
 
-        val directions = arrayOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)
+        val dr = intArrayOf(-1, 1, 0, 0)
+        val dc = intArrayOf(0, 0, -1, 1)
 
         while (pq.isNotEmpty()) {
-            val curState = pq.poll()
-            val time = curState.time
-            val r = curState.r
-            val c = curState.c
-            val parity = curState.parity
+            val cur = pq.poll()
+            val time = cur.time
+            val r = cur.r
+            val c = cur.c
+            val parity = cur.parity
 
-            if (time > dist[r][c][parity]) continue
-
+            val dIdx = (r * m + c) * 2 + parity
+            if (time > dist[dIdx]) continue
             if (r == n - 1 && c == m - 1) return time
 
             val nextParity = 1 - parity
-            val moveDuration = if (parity == 0) 1 else 2
+            val duration = if (parity == 0) 1 else 2
 
-            for ((dr, dc) in directions) {
-                val nr = r + dr
-                val nc = c + dc
+            for (i in 0..3) {
+                val nr = r + dr[i]
+                val nc = c + dc[i]
+                if (nr in 0 until n && nc in 0 until m) {
+                    val nDIdx = (nr * m + nc) * 2 + nextParity
+                    val constraint = moveTime[nr][nc]
+                    val start = if (time > constraint) time else constraint
+                    val arrival = start + duration
 
-                if (nr in 0..<n && nc in 0..<m) {
-                    val startTime = maxOf(time, moveTime[nr][nc])
-                    val arrivalTime = startTime + moveDuration
-
-                    if (arrivalTime < dist[nr][nc][nextParity]) {
-                        dist[nr][nc][nextParity] = arrivalTime
-                        pq.add(State(arrivalTime, nr, nc, nextParity))
+                    if (arrival < dist[nDIdx]) {
+                        dist[nDIdx] = arrival
+                        pq.add(State(arrival, nr, nc, nextParity))
                     }
                 }
             }
